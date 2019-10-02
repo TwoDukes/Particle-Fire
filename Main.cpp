43df648a9fa61ffa7f4af84d8e0a8a26dc8a929c
Dustin Podell <magicdustin14@gmail.com>
2019-10-02 03:17:59 -0700

    Added basic color shifting animation in update loop

--- Main.cpp
+++ Main.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include "SDL2/SDL.h"
+#include <math.h>
 #include "Screen.h"
 
 
@@ -27,16 +28,18 @@ int main()
   while (true)
   {
     // Update Particles
+    int elapsedMillis = SDL_GetTicks();
+    unsigned char red = (unsigned char)((1 + sin(elapsedMillis * 0.0001)) * 128);
+    unsigned char green = (unsigned char)((1 + sin(elapsedMillis * 0.0002)) * 128);
+    unsigned char blue = (unsigned char)((1 + sin(elapsedMillis * 0.0003)) * 128);
+
 
     for (int y = 0; y < Screen::SCREEN_HEIGHT; y++)
     {
       for(int x = 0; x < Screen::SCREEN_WIDTH; x++){
-        screen.setPixel(x, y, 128, 0, 255);       
+        screen.setPixel(x, y, red, green, blue);       
       }
     }
-    
-
-    screen.setPixel(400, 300, 255, 255, 255);
 
     // Draw the screen
     screen.update();
